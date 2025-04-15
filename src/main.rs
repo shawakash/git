597c8a9b738d9dfe81b808466ccdb2aea4f9b6ea
Akash Shaw <shawakash2003@gmail.com>
2025-04-15 19:32:48 +0530

    feat: impl the cat file command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
-use std::fs;
+use flate2::read::ZlibDecoder;
+use std::{
+    ffi::CStr,
+    fs,
+    io::{BufRead, BufReader, Read},
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,6 +17,18 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Init,
+
+    CatFile {
+        #[clap(short = 'p')]
+        preety_print: bool,
+
+        object_hash: String,
+    },
+}
+
+#[derive(Debug)]
+enum Kind {
+    Blob,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -24,6 +42,53 @@ fn main() -> anyhow::Result<()> {
                 fs::create_dir(".git/refs").unwrap();
                 fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
                 println!("Initialized empty Git repository in .git/");
+            }
+            Commands::CatFile {
+                preety_print,
+                object_hash,
+            } => {
+                anyhow::ensure!(preety_print, "mode should contain -p");
+                let path = format!(".git/objects/{}/{}", &object_hash[..2], &object_hash[2..]);
+                let file = fs::File::open(&path).context("open in .git/objects")?;
+
+                // Convert the file to a ZlibDecoder
+                let z = ZlibDecoder::new(file);
+                let mut z = BufReader::new(z);
+                let mut buf = Vec::new();
+                // Read the header from the ZlibDecoder
+                // The header is expected to be in the format "kind size\0content"
+                z.read_until(0, &mut buf)
+                    .context("read header from .git/objects")?;
+                let header = CStr::from_bytes_with_nul(&buf)
+                    .context("know there is exactly one nul, at the end")?;
+                let header = header
+                    .to_str()
+                    .context(".git/objects file header is not utf-8 ? wtf")?;
+                let Some((kind, size)) = header.split_once(" ") else {
+                    anyhow::bail!("{path} file header did not start with a know type: {header}");
+                };
+                let kind = match kind {
+                    "blob" => Kind::Blob,
+                    _ => {
+                        anyhow::bail!("to be impl for kind: {kind}")
+                    }
+                };
+                let size = size
+                    .parse::<u64>()
+                    .context("parse size from .git/objects header size: {size}")?;
+                let mut z = z.take(size);
+                match kind {
+                    Kind::Blob => {
+                        let stdout = std::io::stdout();
+                        let mut stdout = stdout.lock();
+                        let n = std::io::copy(&mut z, &mut stdout)
+                            .context("write .git/object to stdout")?;
+                        anyhow::ensure!(
+                            n == size,
+                            ".git/objects file was not the expected size: (expected: {size}, actual: {n})"
+                        )
+                    }
+                }
             }
         }
     } else {
